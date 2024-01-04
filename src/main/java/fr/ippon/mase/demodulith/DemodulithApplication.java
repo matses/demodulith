cfd23b20c32e8515b76e8e68302932f1aa6a549e
Mathieu <mathieu.sescosse-ext@ixxitechside.com>
2024-01-04 11:00:27 +0100

    init from spring intializr

---
+++ src/main/java/fr/ippon/mase/demodulith/DemodulithApplication.java
@@ -0,0 +1,13 @@
+package fr.ippon.mase.demodulith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemodulithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemodulithApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/ippon/mase/demodulith/DemodulithApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.ippon.mase.demodulith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemodulithApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
